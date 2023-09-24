43f99438fb9165361ac2be3aab3b8785315bad3a
Zhenya Loyko <e.lojko@inbox.ru>
2023-09-24 14:54:08 +0300

    Designed

---
+++ String Catalog/String Catalog/Models/Feedback.swift
@@ -0,0 +1,14 @@
+//
+//  Feedback.swift
+//  String Catalog
+//
+//  Created by Евгений Лойко on 24.09.23.
+//
+
+import Foundation
+
+struct Feedback {
+    private let date = Date()
+    let text: String
+    let mark: Double
+}
---
+++ String Catalog/String Catalog/Models/ViolinString.swift
@@ -0,0 +1,15 @@
+//
+//  String.swift
+//  String Catalog
+//
+//  Created by Евгений Лойко on 24.09.23.
+//
+
+import UIKit
+
+struct ViolinString {
+    let name: String
+    let price: Double
+    let image: UIImage?
+    var feedBacks: [Feedback] = []
+}
